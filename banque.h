f69f72dfaff775f5aa7582744d02e330a11e0265
Nawres Chaabane <nawres.chaabane@ensi-uma.tn>
2021-04-11 22:37:15 +0100

    Initialize project

---
+++ banque.h
@@ -0,0 +1,51 @@
+#ifndef BANQUE_H
+#define BANQUE_H
+#include <iostream>
+
+using namespace std;
+class banque
+{
+
+    private:
+        int montant, nb_hotels, nb_maisons ;
+
+    public:
+        banque(){ montant= 12000;
+        nb_maisons= 32;
+        nb_hotels=12;}
+
+
+        ~banque(){}
+        int get_nb_maisons (){
+            return nb_maisons;
+
+        }
+        int get_nb_hotels() {
+            return nb_hotels;
+        }
+        void set_nb_maisons(int a){
+            nb_maisons-=a;
+
+
+        }
+        void set_nb_hotels(int b){
+            nb_hotels-=b;
+
+
+        }
+        int donner_arg(int arg,joueur j){
+            montant= montant - arg;
+            j.solde+=arg;
+            return (montant);
+
+        }
+            int recevoir_arg(int arg,joueur j){
+            montant= montant + arg;
+            j.solde-=arg;
+            return (montant);
+
+        }
+
+};
+
+#endif // BANQUE_H
